8bd0d37c42a6200d4af0cdd8381c14a09ecfd3eb
peachkichi <peachkichi@peachkichinoMacBook-Pro.local>
2017-11-29 19:49:32 +0900

    csv完了か？

--- app/controllers/beans_controller.rb
+++ app/controllers/beans_controller.rb
@@ -8,36 +8,36 @@ class BeansController < ApplicationController
   # @dates = Bean.pluck(:id, :acidity, :sweetness, :bitter, :richness, :aroma)
 
   # beanscsvファイルのデータをインポート
-    CSV.foreach("test.csv") do |csv|
-      next if csv[0] == "id"
-      # next if csv[0] == "1"
-     Bean.create!(name: csv[1], taste: csv[2], acidity: csv[3].to_i, sweetness: csv[4].to_i, bitter: csv[5].to_i, richness: csv[6].to_i, aroma: csv[7].to_i, place: csv[8], feature: csv[9], geography_history: csv[10])
-    end
+  #   CSV.foreach("test.csv") do |csv|
+  #     next if csv[0] == "id"
+  #     next if csv[0] == "1"
+  #    Bean.create!(name: csv[1], taste: csv[2], acidity: csv[3].to_i, sweetness: csv[4].to_i, bitter: csv[5].to_i, richness: csv[6].to_i, aroma: csv[7].to_i, place: csv[8], feature: csv[9], geography_history: csv[10])
+  #   end
   # paramsにidがあれば計算する
-  # if params[:bean]
-  #
-  #   beans = Bean.average_taste(params[:bean][:id])
-  #   @beans = Bean.init_chart(beans)
-  #   render :result
-  #
-  # else
-  #   @bean = Bean.new
-  #   @beans = Bean.all.order("id").map{|b|
-  #     {id: b.id,
-  #      name: b.name,
-  #      taste: b.taste,
-  #      place: b.place,
-  #      feature: b.feature,
-  #      geography_history: b.geography_history,
-  #      acidity: b.acidity,
-  #      sweetness: b.sweetness,
-  #      bitter: b.bitter,
-  #      richness: b.richness,
-  #      aroma: b.aroma,
-  #      lazy_chart_data: Bean.init_chart(b.chart_data),
-  #     }
-  #   }
-  # end
+  if params[:bean]
+
+    beans = Bean.average_taste(params[:bean][:id])
+    @beans = Bean.init_chart(beans)
+    render :result
+
+  else
+    @bean = Bean.new
+    @beans = Bean.all.order("id").map{|b|
+      {id: b.id,
+       name: b.name,
+       taste: b.taste,
+       place: b.place,
+       feature: b.feature,
+       geography_history: b.geography_history,
+       acidity: b.acidity,
+       sweetness: b.sweetness,
+       bitter: b.bitter,
+       richness: b.richness,
+       aroma: b.aroma,
+       lazy_chart_data: Bean.init_chart(b.chart_data),
+      }
+    }
+  end
   # paramsにidがない場合
 
 
